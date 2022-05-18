4cafbd1d049d62f23bb7e3b090d66012915b0882
OleksandrHorzhij <horzhij@gmail.com>
2022-05-18 11:42:20 +0300

    fix mobile btn for model window

--- src/modal.js
+++ src/modal.js
@@ -3,10 +3,14 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    mobileModalLinks: document.querySelectorAll('[mobile-modal-link]'),
+
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.mobileModalLinks.forEach(elem => elem.addEventListener('click', toggleModal));
+
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
