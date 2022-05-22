47ca1b91ce551ac7011244816350696ef50d3990
Kateryna Skoryna <k.skoryna@gmail.com>
2022-05-22 18:36:46 +0200

    New script
    Проверь сразу, чтоб оно работало, плиз

---
+++ src/read-more.js
@@ -0,0 +1,11 @@
+const readMoreBtn = document.querySelector(".review__btn");
+const text = document.querySelector(".review__comment");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Hide") {
+    readMoreBtn.innerText = "Read More";
+  } else {
+    readMoreBtn.innerText = "Hide";
+  }
+});
